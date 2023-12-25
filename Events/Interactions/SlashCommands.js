9dce9d8a4e4b65e47d94b8ea694e402c5d8b4706
Kiiya <105589176+KKiiya@users.noreply.github.com>
2023-12-25 08:43:02 +0100

    Update SlashCommands.js

--- Events/Interactions/SlashCommands.js
+++ Events/Interactions/SlashCommands.js
@@ -7,15 +7,29 @@ module.exports = {
      * @param {ChatInputCommandInteraction} interaction
     */
     execute(interaction, client) {
-        if(!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
+            const command = client.commands.get(interaction.commandName);
+            if(!command) 
+            return interaction.reply({
+                content: "This command is outdated.",
+                ephemeral: true
+            });
 
-        const command = client.commands.get(interaction.commandName);
-        if(!command) 
-        return interaction.reply({
-            content: "This command is outdated.",
-            ephemeral: true
-        });
+            command.execute(interaction, client);
 
-        command.execute(interaction, client);
+        } else if (interaction.isButton()) {
+            const buttons = client.buttons;
+            const button = buttons.get(interaction.customId)
+
+            if(!button) 
+
+            return interaction.reply({
+                content: "This button is outdated.",
+                ephemeral: true
+            });
+            button.execute(interaction, client)
+        } else {
+            return;
+        }
     }
 }   
