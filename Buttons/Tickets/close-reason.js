f0ea4e9e9d766c05fbb2a0393038f44c9f509eb0
Kiiya <105589176+KKiiya@users.noreply.github.com>
2023-12-27 19:28:16 +0100

    Made close buttons save data

--- Buttons/Tickets/close-reason.js
+++ Buttons/Tickets/close-reason.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ButtonInteraction } = require('discord.js');
+const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ButtonInteraction, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 
 module.exports = {
@@ -17,6 +17,21 @@ module.exports = {
         const ticketCategory = guildData.ticketsCategory;
         const ticketCreationChannel = guildData.ticketsCreationChannel;
         const ticket = interaction.channel;
+        const claimedBy = guildData.tickets[ticket.id].claimedBy;
+        let finalField;
+        if (claimedBy == null) {
+            finalField = {
+                name: "Claimed by",
+                value: "Not claimed",
+                inline: true
+            }
+        } else {
+            finalField = {
+                name: "Claimed by",
+                value: `<@${claimedBy}>`,
+                inline: true
+            }
+        }
 
         if (ticket.parentId !== ticketCategory) return interaction.reply({ content: "This is not a ticket!", ephemeral: true });
         if (ticket.id === ticketCreationChannel) return interaction.reply({ content: "You can't close the ticket creation channel!", ephemeral: true });
@@ -41,7 +56,49 @@ module.exports = {
         interaction
             .awaitModalSubmit({ filter, time: 30_000 })
             .then((modalInteraction) => {
-                const reason = modalInteraction.fields.getTextInputValue("closeReason");
+                const reason = modalInteraction.fields.getTextInputValue("closeReason") || "No reason provided.";
+
+                guildData.ticketsLogChannel = guildData.ticketsLogChannel || null;
+                const channel = interaction.guild.channels.cache.get(guildData.ticketsLogChannel);
+                if (channel) {
+                    const embed = new EmbedBuilder();
+                    embed.setTitle("Ticket Closed");
+                    embed.setTimestamp(Date.now());
+                    embed.setColor(0xFF0000);
+                    embed.addFields(
+                        {
+                            name: "Ticket ID",
+                            value: `${guildData.tickets[ticket.id].count}`,
+                            inline: true
+                        },
+                        {
+                            name: "Created by",
+                            value: `<@${guildData.tickets[ticket.id].user}>`,
+                            inline: true
+                        },
+                        {
+                            name: "Closed by",
+                            value: `<@${interaction.user.id}>`,
+                            inline: true
+                        },
+                        {
+                            name: "Reason",
+                            value: reason,
+                            inline: true
+                        },
+                        {
+                            name: "Creation Date",
+                            value: `<t:${ticket.createdAt.getMilliseconds()}:F>`,
+                            inline: true
+                        },
+                        {
+                            name: "Closed Date",
+                            value: `<t:${Date.now()}:F>`,
+                            inline: true
+                        }, finalField);
+                    embed.setThumbnail(interaction.guild.iconURL());
+                    channel.send({ embeds: [embed] });
+                }
 
                 ticket.delete(reason);
 
--- Buttons/Tickets/close.js
+++ Buttons/Tickets/close.js
@@ -17,11 +17,68 @@ module.exports = {
         const ticketCategory = guildData.ticketsCategory;
         const ticketCreationChannel = guildData.ticketsCreationChannel;
         const ticket = interaction.channel;
+        const reason = "No reason provided.";
+        const claimedBy = guildData.tickets[ticket.id].claimedBy;
+        let finalField;
+        if (claimedBy == null) {
+            finalField = {
+                name: "Claimed by",
+                value: "Not claimed",
+                inline: true
+            }
+        } else {
+            finalField = {
+                name: "Claimed by",
+                value: `<@${claimedBy}>`,
+                inline: true
+            }
+        }
 
         if (ticket.parentId !== ticketCategory) return interaction.reply({ content: "This is not a ticket!", ephemeral: true });
         if (ticket.id === ticketCreationChannel) return interaction.reply({ content: "You can't close the ticket creation channel!", ephemeral: true });
 
-        ticket.delete();
-        interaction.reply({ content: "Ticket closed!", ephemeral: true });
+        guildData.ticketsLogChannel = guildData.ticketsLogChannel || null;
+        const channel = interaction.guild.channels.cache.get(guildData.ticketsLogChannel);
+        if (channel) {
+            const embed = new EmbedBuilder();
+            embed.setTitle("Ticket Closed");
+            embed.setTimestamp(Date.now());
+            embed.setColor(0xFF0000);
+            embed.addFields(
+                {
+                    name: "Ticket ID",
+                    value: `${guildData.tickets[ticket.id].count}`,
+                    inline: true
+                },
+                {
+                    name: "Created by",
+                    value: `<@${guildData.tickets[ticket.id].user}>`,
+                    inline: true
+                },
+                {
+                    name: "Closed by",
+                    value: `<@${interaction.user.id}>`,
+                    inline: true
+                },
+                {
+                    name: "Reason",
+                    value: reason,
+                    inline: true
+                },
+                {
+                    name: "Creation Date",
+                    value: `<t:${ticket.createdAt.getMilliseconds()}:F>`,
+                    inline: true
+                 },
+                {
+                    name: "Closed Date",
+                    value: `<t:${Date.now()}:F>`,
+                    inline: true
+                }, finalField);
+            embed.setThumbnail(interaction.guild.iconURL());
+            channel.send({ embeds: [embed] });
+        }
+        ticket.delete(reason);
+        interaction.reply({ content: "Closed the ticket!", ephemeral: true });
     }
 }
